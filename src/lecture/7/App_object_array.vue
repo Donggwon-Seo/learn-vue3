71801e2d771e2b086d9f148bdc2983f45b097316
SeoDonggwon <ehdrnjsdl920@naver.com>
2023-02-16 17:49:37 +0900

    Template Syntax

---
+++ src/lecture/7/App_object_array.vue
@@ -0,0 +1,30 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+
+export default {
+	setup() {
+		// ref-> Object
+		const count = ref(0);
+		const state = reactive({
+			count,
+		});
+		console.log(state.count);
+		console.log(count.value);
+		count.value++;
+
+		// ref-> Array
+		const message = ref("Hello");
+		const arr = reactive([message]);
+		console.log("arr[0]: ", arr[0].value);
+
+		// Destructuring
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
