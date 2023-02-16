c2b9f9b79c33b4df2e90fd6f93bd646a85f6c9dd
SeoDonggwon <ehdrnjsdl920@naver.com>
2023-02-16 17:57:31 +0900

    toRef, readonly 코드 추가

---
+++ src/lecture/7/App_readonly.vue
@@ -0,0 +1,22 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, readonly } from "vue";
+
+export default {
+	setup() {
+		const original = reactive({ count: 0 });
+
+		const copy = readonly(original);
+
+		// 원본이 변이되면 복사본에 의존하는 watch 도 트리거될 것 입니다.
+		original.count++;
+
+		return { copy };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
